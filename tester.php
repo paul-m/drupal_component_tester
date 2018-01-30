7212444a0bf3ea149c89a83e256788be0962ba57
Paul Mitchum <paul@mile23.com>
2018-01-29 20:24:19 -0800

    more components...

--- tester.php
+++ tester.php
@@ -20,10 +20,12 @@ $loader = require __DIR__ . '/vendor/autoload.php';
 
 $components = [
   // 'package/name' => 'Group',
-  'drupal/core-annotation' => 'Annotation',
+//  'drupal/core-annotation' => 'Annotation',
 //  'drupal/core-assertion' => 'Assertion',
-  //'drupal/core-bridge' => 'Bridge',
-  //'drupal/core-class-finder' => 'ClassFinder',
+// FAILING  'drupal/core-bridge' => 'Bridge',
+//  'drupal/core-class-finder' => 'ClassFinder',
+  'drupal/core-datetime' => 'Datetime',
+  'drupal/core-dependency-injection' => 'DependencyInjection',
 ];
 
 $fs = new Filesystem();
@@ -33,14 +35,18 @@ $my_dir = __DIR__;
 foreach ($components as $package => $group) {
   echo "\n===---> Building package: $package\n\n";
   $path = 'core/lib/Drupal/Component/' . $group;
-  // Move the component to the workspace.
+
   chdir($my_dir);
+
+  // Move the component to the workspace.
   $fs->mirror(
     __DIR__ . '/drupal/' . $path,
     __DIR__ . '/workspace/component'
   );
   // Get to work.
   chdir($my_dir . '/workspace');
+  // Clean up.
+  $fs->remove(['composer.json', 'composer.lock']);
   // Write out a composer.json to the workspace.
   file_put_contents(
     'composer.json', json_encode(
@@ -49,22 +55,24 @@ foreach ($components as $package => $group) {
     )
   );
 
-  // Spare composer install so wikimedia merge catches the dependencies.
-  $composer_command = 'composer install --no-progress --no-suggest';
-  echo "\n\n-> $composer_command\n\n";
+  // Ensure there are no leftover vendor artifacts.
+  $fs->remove(['vendor', 'composer.lock']);
+
+  $composer_command = 'composer update --lock --no-progress --no-suggest';
+  echo "\n\nPRIMING -> $composer_command\n\n";
   echo shell_exec($composer_command);
 
   // Composer update and run tests.
   foreach (['--prefer-lowest', ' '] as $argument) {
-    $composer_command = 'composer update --no-progress --no-suggest ' . $argument;
-    echo "\n\n-> $composer_command\n\n";
+    $composer_command = 'composer update --lock --no-progress --no-suggest ' . $argument;
+    echo "\n\nCOMPOSER -> $composer_command\n\n";
     echo shell_exec($composer_command);
 
     $phpunit_command = './vendor/bin/phpunit core/tests/Drupal/Tests/Component/' . $group;
-    echo "\n\n-> $phpunit_command\n\n";
+    echo "\n\nPHPUNIT -> $phpunit_command\n\n";
     echo shell_exec($phpunit_command);
   }
-//  $fs->remove(['component', 'composer.lock']);
+  $fs->remove(['component']);
 }
 
 echo "\n\nDone.\n\n";
@@ -100,6 +108,10 @@ function buildComposerArray($component_package, $group, $path) {
       'psr-4' => [
         "Drupal\\Tests\\Component\\$group\\" => "core/tests/Drupal/Tests/Component/$group",
       ],
+    ],
+    'respositories' => [
+      'type' => 'composer',
+      'url' => 'https://packages.drupal.org/8',
     ],
   ];
 }
